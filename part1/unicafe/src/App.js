f56c36ee7a2a1be16cabd2eb2c1a4f336ec1a96e
Azryel <AzryelShroud@gmail.com>
2021-07-01 17:07:17 +0200

    step2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -2,12 +2,17 @@ import React, { useState } from 'react'
 
 const Statistics = ({good, neutral, bad}) => {
 
+  let all = good + neutral + bad;
+
   return(
     <>
       <h1> Statistics </h1>
       <p> Good {good} </p>
       <p> Neutral {neutral} </p>
       <p> Bad {bad} </p>
+      <p> All {all} </p>
+      <p> Average {(good - bad)/all} </p>
+      <p> Positive {good/all *100}% </p>
     </>
   )
 }
