b4672d4badcf88a0efb3929f7fbe086538782eda
Azryel <AzryelShroud@gmail.com>
2021-07-04 18:41:36 +0200

    step3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,7 +4,7 @@ const Numbers = ({persons}) => {
   return (
     persons.map( person => {
       return(
-        <p key={person.name}> {person.name} </p>
+        <p key={person.name}> {person.name} {person.number} </p>
       )
     } )
   )
@@ -12,9 +12,10 @@ const Numbers = ({persons}) => {
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: "123-4567" }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState(0)
 
   function addToPhonebook(e) {
     e.preventDefault();
@@ -26,6 +27,7 @@ const App = () => {
 
     const personObj = {
       name: newName,
+      number: newNumber,
     }
 
     setPersons(persons.concat(personObj));
@@ -35,13 +37,21 @@ const App = () => {
     setNewName(e.target.value);
   }
 
+  function handleNumberChange(e) {
+    setNewNumber(e.target.value);
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
       <div>debug: {newName}</div>
+      <div>debug: {newNumber}</div>
       <form onSubmit={addToPhonebook}>
         <div>
           name: <input onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
