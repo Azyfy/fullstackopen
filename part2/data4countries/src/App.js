19af95dc56bc71af9e426538da593f8d712b3e9e
Azryel <AzryelShroud@gmail.com>
2021-07-04 21:53:42 +0200

    data4countries react

---
+++ part2/data4countries/src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        Data for countries
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
