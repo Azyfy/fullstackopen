a34e9a117c4bc4cfd50c69937733bae4125c4528
Azryel <AzryelShroud@gmail.com>
2021-07-04 21:39:33 +0200

    step6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Numbers = ({persons, filter}) => {
   return (
@@ -44,15 +45,20 @@ const Filter = ({handleFilterChange}) => {
 }
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [ persons, setPersons ] = useState([]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState("")
   const [ filter, setFilter ] = useState("")
+
+  useEffect(() => {
+    console.log('effect')
+    axios.get("http://localhost:3001/persons")
+        .then( response => {
+          console.log(response.data)
+          setPersons(response.data)
+        })
+    
+  }, [])
 
   function addToPhonebook(e) {
     e.preventDefault();
