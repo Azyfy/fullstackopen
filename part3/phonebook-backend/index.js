a39bd6c1f7e8f6fb50d42dc147e3dd55d0fb57df
Azryel <AzryelShroud@gmail.com>
2021-07-21 15:18:41 +0200

    step5

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -2,6 +2,8 @@ const { response } = require("express")
 const express = require("express")
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     { 
         "id": 1,
@@ -55,6 +57,21 @@ app.delete("/api/persons/:id", (request, response) => {
     persons = persons.filter( person => person.id != id )
 
     response.status(204).end()
+})
+
+app.post("/api/persons", (request, response) => {
+    const body = request.body
+    const id = Math.floor(Math.random() * 1000000)
+    
+    const person = {
+        id,
+        name: body.name,
+        number: body.number
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
 })
 
 
