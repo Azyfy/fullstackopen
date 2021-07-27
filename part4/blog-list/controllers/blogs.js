96825a8f661626cae5c6c86516ac86fd1b98b06e
Azryel <AzryelShroud@gmail.com>
2021-07-27 22:03:58 +0200

    4.8 blog list tests step1

--- part4/blog-list/controllers/blogs.js
+++ part4/blog-list/controllers/blogs.js
@@ -1,15 +1,15 @@
 const blogsRouter = require("express").Router()
 const Blog = require("../models/blog")
 
-blogsRouter.get('/', (request, response) => {
-    Blog
-      .find({})
-      .then(blogs => {
-        response.json(blogs)
-      })
+blogsRouter.get('/', async (request, response) => {
+  
+  const blogs = await Blog.find({})  
+  
+  response.json(blogs)
+
   })
   
-  blogsRouter.post('/', (request, response) => {
+blogsRouter.post('/', (request, response) => {
     const blog = new Blog(request.body)
   
     blog
@@ -19,4 +19,4 @@ blogsRouter.get('/', (request, response) => {
       })
   })
 
-  module.exports = blogsRouter
+module.exports = blogsRouter
