55f626925a551e946eca9a04538e88ceb8aff3a6
Azryel <AzryelShroud@gmail.com>
2021-07-27 16:48:39 +0200

    4.4 step2

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -2,6 +2,16 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+
+    const likes = blogs.map( obj => obj.likes  )
+
+    const total = likes.reduce( (total, number) => total + number )
+
+    return total
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
   }
