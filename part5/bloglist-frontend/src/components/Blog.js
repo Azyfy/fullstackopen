865716b95c6de6048fcd3c09f942dfab6aa03aa3
Azryel <AzryelShroud@gmail.com>
2021-07-30 22:46:25 +0200

    5.7 step7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,29 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
+import React, { useState } from 'react'
+const Blog = ({blog}) => {
+  const [ toggle, setToggle ] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const dislplayVisible = { display: toggle ? "" : "none" }
+
+  return (
+  <div style={blogStyle} >
+    {blog.title} {blog.author} 
+    <button onClick={() => setToggle(!toggle) } > {toggle ? "Hide" : "View" } </button>
+
+    <div style={ dislplayVisible } >
+      <p> {blog.url} </p>
+      <p> {blog.likes} <button>Like</button> </p>
+      <p> {blog.user.name} </p>
+    </div>
   </div>  
 )
+  }
 
 export default Blog
