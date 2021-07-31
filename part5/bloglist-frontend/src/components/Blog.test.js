a8dd6e0be8cbb29d774a3fe7dbe5f0a2c508ca03
Azryel <AzryelShroud@gmail.com>
2021-07-31 19:31:03 +0200

    5.14 step2

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,6 +1,7 @@
 import React from "react"
 import "@testing-library/jest-dom/extend-expect"
-import { render } from "@testing-library/react"
+import { render, fireEvent } from "@testing-library/react"
+import { prettyDOM } from "@testing-library/dom"
 import Blog from "./Blog"
 
 test("renders title and author", () => {
@@ -33,5 +34,42 @@ test("renders title and author", () => {
         "Frontend Post"
   )
 
+  // component.debug()
+  // console.log(prettyDOM(div))
 
+})
+
+test("clicking the button shows more details", () => {
+    const blog = {
+        title: "Frontend Post",
+        author: "Pengu",
+        url: "some page",
+        likes: 3,
+        user: {
+            name: "Pengu-san",
+        },
+    }
+
+    const loggedUser = {
+        name: "NO",
+    }
+
+    const mockHandler = jest.fn()
+
+    const component = render(
+        <Blog  blog={blog}  loggedUser={loggedUser} />
+    )
+
+    const button = component.getByText("View")
+     fireEvent.click(button)
+
+  //   console.log(prettyDOM(button))
+  //   component.debug()
+
+     expect(component.container).toHaveTextContent(
+        "some page"
+    )
+    expect(component.container).toHaveTextContent(
+        3
+    )
 })
