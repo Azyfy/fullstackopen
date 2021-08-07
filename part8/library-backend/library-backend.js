6358c06696382926d3101c80e5e922e8bcc07e42
Azryel <AzryelShroud@gmail.com>
2021-08-07 21:56:53 +0200

    8.4 books of an author

--- part8/library-backend/library-backend.js
+++ part8/library-backend/library-backend.js
@@ -113,7 +113,7 @@ const typeDefs = gql`
   type Query {
       bookCount: Int!
       authorCount: Int!
-      allBooks: [Book!]!
+      allBooks(author: String!): [Book!]!
       allAuthors: [Person]
   }
 `
@@ -130,7 +130,7 @@ const resolvers = {
   Query: {
       bookCount: () => books.length,
       authorCount: () => authors.length,
-      allBooks: () => books,
+      allBooks: (root, args) => books.filter(book => book.author === args.author),
       allAuthors: () => authors,
   }
 }
