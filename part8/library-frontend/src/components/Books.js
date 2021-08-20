9770f7b3a0279e082d7460f3a85c06cc56873f00
Azryel <AzryelShroud@gmail.com>
2021-08-20 20:55:49 +0200

    8.19 books by genre part1

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,16 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Select from 'react-select';
 
 const Books = (props) => {
+  const [selectedGenre, seSelectedGenre] = useState({value: "All genres"})
+  
   if (!props.show) {
     return null
   }
 
   const books = props.books
+  
+  let genres = ["All genres"]
+  books.forEach(book => {
+    const array = book.genres.filter(genre => !genres.includes(genre))
+   
+    genres = [...genres, ...array]
+
+  })
+
+  const options = genres.map(genre => {
+    return { value: genre, label: genre } 
+})
+
+let filteredBooks = books
+if(selectedGenre.value === "All genres") {
+  filteredBooks = books
+  }
+  else {
+    filteredBooks = books.filter(book => book.genres.includes(selectedGenre.value))
+  }
 
   return (
     <div>
       <h2>books</h2>
-
+      In genre { selectedGenre.value }
       <table>
         <tbody>
           <tr>
@@ -22,7 +45,7 @@ const Books = (props) => {
               published
             </th>
           </tr>
-          {books.map(a =>
+          {filteredBooks.map(a =>
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
@@ -31,6 +54,11 @@ const Books = (props) => {
           )}
         </tbody>
       </table>
+      <Select
+                    defaultValue={selectedGenre}
+                    onChange={seSelectedGenre}
+                    options={options}
+                />
     </div>
   )
 }
