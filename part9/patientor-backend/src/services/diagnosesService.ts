0043e8d224772d98b18273790dade01aee3f8487
Azryel <AzryelShroud@gmail.com>
2021-08-23 22:34:56 +0200

    9.10 step3

---
+++ part9/patientor-backend/src/services/diagnosesService.ts
@@ -0,0 +1,13 @@
+import diagnosesData from "../../data/diagnoses.json"
+
+import { DiagnosesEntry } from "../types"
+
+const diagnoses: Array<DiagnosesEntry> = diagnosesData;
+
+const getEntries = () => {
+    return diagnoses
+}
+
+export default {
+    getEntries,
+}
