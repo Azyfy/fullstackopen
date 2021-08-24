df91151dbc4c485065a6561d02b35e3669ea8f81
Azryel <AzryelShroud@gmail.com>
2021-08-24 21:22:54 +0200

    9.14

---
+++ part9/react-app-ts/src/components/Total.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Total = ({ total }: { total:number }) => {
+
+    return (
+        <p>
+            Number of exercises{" "}
+            {total}
+        </p>
+    );
+};
+
+export default Total;
